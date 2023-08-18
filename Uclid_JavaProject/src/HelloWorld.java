86eb9ad4189433cd468794e44949af90750aafa2
sudheer51 <sudheer694@gmail.com>
2023-08-18 06:59:56 +0530

    Update HelloWorld.java

--- Uclid_JavaProject/src/HelloWorld.java
+++ Uclid_JavaProject/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("updating the file");
+		System.out.println("updating the from master");
 		System.out.println("Fixing the bug-1234");
 		
 		
