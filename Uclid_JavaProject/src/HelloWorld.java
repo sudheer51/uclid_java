471df6dbd378b0bb07711943da7a6457bf58010a
sudheer51 <sudheer694@gmail.com>
2023-08-18 06:53:45 +0530

    deleted lines

--- Uclid_JavaProject/src/HelloWorld.java
+++ Uclid_JavaProject/src/HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("updating the file");
-		System.out.println("Hello World- Selenium Project Batch");
 		System.out.println("Fixing the bug-1234");
 		
 		
